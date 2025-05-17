38b0bcad05ecea663bbd4eafd72578e591fa02d8
eunji <oieunxe@gmail.com>
2025-05-18 04:30:49 +0900

    feat: 취미 Response Model 구현

---
+++ SOPKATHON36/SOPKATHON36/Network/Model/HobbyResponse.swift
@@ -0,0 +1,54 @@
+//
+//  HobbyResponse.swift
+//  SOPKATHON36
+//
+//  Created by 이은지 on 5/18/25.
+//
+
+import Foundation
+
+struct HobbyResponse: Decodable {
+    let statusCode: Int
+    let message: String
+    let data: ContentData
+
+    enum CodingKeys: String, CodingKey {
+        case statusCode = "status_code"
+        case message
+        case data
+    }
+}
+
+extension HobbyResponse {
+    struct ContentData: Decodable {
+        let contents: [Content]
+    }
+}
+
+extension HobbyResponse.ContentData {
+    struct Content: Decodable {
+        let contentID: Int
+        let image: String
+        let location: String
+        let description: String
+
+        enum CodingKeys: String, CodingKey {
+            case contentID = "content_id"
+            case image
+            case location
+            case description
+        }
+    }
+}
+
+extension HobbyResponse {
+    func toEntity() -> [PlaceModel] {
+        return data.contents.map {
+            .init(
+                placeImage: $0.image,
+                placeName: $0.location,
+                placeDetail: $0.description
+            )
+        }
+    }
+}
