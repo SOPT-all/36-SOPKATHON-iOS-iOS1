746746d564b1bea33844ccb145cf481a43122097
hooni <jhlee909090@gmail.com>
2025-05-18 01:26:50 +0900

    feat: #16 뷰 익스텐션

--- SOPKATHON36/SOPKATHON36/Resource/Extension/UIView+.swift
+++ SOPKATHON36/SOPKATHON36/Resource/Extension/UIView+.swift
@@ -1,7 +1,26 @@
 //
 //  UIView+.swift
-//  SOPKATHON36
 //
-//  Created by 최주리 on 5/16/25.
+//  Created by 이지훈 on 6/30/24.
 //
 
+import UIKit
+
+extension UIView {
+    convenience init(backgroundColor: UIColor) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+    }
+    
+    func addSubviews(_ views: UIView...) {
+        views.forEach {
+            addSubview($0)
+        }
+    }
+    
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+}
